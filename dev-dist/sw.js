85c6a8f2bd495fe3d18b789fd13128a5670ee263
Mmoolchandji <moolchand.jain9537@gmail.com>
2025-08-08 10:01:26 +0530

    refactor: Remove duplicate custom PWA install prompt
    This commit removes the `InstallPrompt.tsx` component, which was responsible for displaying a custom UI for PWA installation.
    
    By deleting the component and its usage in `main.tsx`, the application will now rely on the browser's native installation prompt. This simplifies the codebase and streamlines the user experience. The service worker and dependency files have been updated accordingly.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.u81i7s9nmpo"
+    "revision": "0.e8v7l4fno9g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { registerPwa } from './utils/pwa.ts'
-import InstallPrompt from './components/PWA/InstallPrompt.tsx'
-import OfflineIndicator from './components/PWA/OfflineIndicator.tsx'
 import React from 'react'
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-    <InstallPrompt />
-    <OfflineIndicator />
   </React.StrictMode>
 );
 
